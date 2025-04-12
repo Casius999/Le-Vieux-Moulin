b0459f94f044b446390c6dc6d7dd86a938892f4b
Casius999 <casius4126@gmail.com>
2025-04-13 03:18:38 +0700

    Ajout du fichier d'initialisation pour le gestionnaire de promotions

---
+++ marketing/recipe_suggestion/src/promotion_manager/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module de gestion des promotions pour Le Vieux Moulin.
+
+Ce package gère la création, la personnalisation et la
+diffusion des promotions basées sur les suggestions de recettes.
+"""
+
+from .promotion_service import PromotionManager
+
+__all__ = ['PromotionManager']
