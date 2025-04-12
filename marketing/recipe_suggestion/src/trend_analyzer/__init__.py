88010f4f18e3bf4f4ebbdb3933ae70f6ad0f4f4b
Casius999 <casius4126@gmail.com>
2025-04-13 03:18:22 +0700

    Ajout du fichier d'initialisation pour l'analyseur de tendances

---
+++ marketing/recipe_suggestion/src/trend_analyzer/__init__.py
@@ -0,0 +1,11 @@
+"""
+Module d'analyse des tendances pour Le Vieux Moulin.
+
+Ce package contient les outils pour analyser les tendances
+clients, les préférences locales et les données de vente
+afin d'identifier les opportunités culinaires.
+"""
+
+from .trend_service import TrendAnalyzer
+
+__all__ = ['TrendAnalyzer']
