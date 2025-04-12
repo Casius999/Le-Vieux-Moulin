de79c711616539b3412e1c9ef40c71e9fd6a4388
Casius999 <casius4126@gmail.com>
2025-04-13 03:18:46 +0700

    Ajout du fichier d'initialisation principal

---
+++ marketing/recipe_suggestion/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module de suggestion de recettes dynamiques pour Le Vieux Moulin.
+
+Ce package analyse les promotions des fournisseurs et les tendances
+clients pour générer automatiquement des suggestions de recettes
+et des promotions adaptées.
+"""
+
+__version__ = '1.0.0'
