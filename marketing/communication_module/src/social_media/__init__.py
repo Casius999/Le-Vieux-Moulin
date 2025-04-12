955c2f9df25bd518d9e7479211d72b89654f2802
Casius999 <casius4126@gmail.com>
2025-04-13 03:37:40 +0700

    Création du module social_media

---
+++ marketing/communication_module/src/social_media/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module de gestion des réseaux sociaux
+
+Ce module gère la publication automatisée sur les différentes plateformes
+de réseaux sociaux, ainsi que l'analyse des performances et la gestion des interactions.
+"""
+
+from .manager import SocialMediaManager
+
+__all__ = ['SocialMediaManager']
