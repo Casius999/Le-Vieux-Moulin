b415162ffed95e4685d03b07f70f0edf6faf9971
Casius999 <casius4126@gmail.com>
2025-04-13 03:37:30 +0700

    Création de la structure de dossiers src et des fichiers d'initialisation

---
+++ marketing/communication_module/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module de Communication et d'Automatisation Marketing - Le Vieux Moulin
+
+Ce module gère l'ensemble des communications marketing et leur automatisation
+pour le restaurant "Le Vieux Moulin".
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Équipe Technique Le Vieux Moulin'
