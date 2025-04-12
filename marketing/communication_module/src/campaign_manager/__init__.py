eb0a2381ea9cc0369493f1b0112aa7c70c0ce8fe
Casius999 <casius4126@gmail.com>
2025-04-13 03:38:08 +0700

    Création du module campaign_manager

---
+++ marketing/communication_module/src/campaign_manager/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module de gestion des campagnes marketing
+
+Ce module gère la création, planification, exécution et suivi des campagnes 
+marketing multicanal pour le restaurant.
+"""
+
+from .manager import CampaignManager
+
+__all__ = ['CampaignManager']
