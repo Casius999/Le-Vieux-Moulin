14a39fdfd14b6ec5214ff1fa26fd074d79a7e35e
Casius999 <casius4126@gmail.com>
2025-04-13 03:38:19 +0700

    Création du module menu_updater

---
+++ marketing/communication_module/src/menu_updater/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module de mise à jour des menus en ligne
+
+Ce module gère la synchronisation automatique des menus du restaurant
+sur toutes les plateformes en ligne (site web, réseaux sociaux, plateformes partenaires).
+"""
+
+from .updater import MenuUpdater
+
+__all__ = ['MenuUpdater']
