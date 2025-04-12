d3d0ce4ccf498d045f490273d27bd11017226e40
Casius999 <casius4126@gmail.com>
2025-04-13 03:18:14 +0700

    Ajout du fichier d'initialisation pour le générateur de recettes

---
+++ marketing/recipe_suggestion/src/recipe_generator/__init__.py
@@ -0,0 +1,11 @@
+"""
+Module de génération de recettes pour Le Vieux Moulin.
+
+Ce package contient les outils nécessaires pour générer
+des suggestions de recettes basées sur les promotions,
+tendances et préférences clients.
+"""
+
+from .generator import RecipeGenerator
+
+__all__ = ['RecipeGenerator']
