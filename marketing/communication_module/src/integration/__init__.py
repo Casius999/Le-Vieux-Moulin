a1ee4d04ee9f35c3b74c4d9bbb40b72efd09abd9
Casius999 <casius4126@gmail.com>
2025-04-18 14:50:21 +0200

    Ajout du fichier d'initialisation pour le module d'intégration

---
+++ marketing/communication_module/src/integration/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module d'intégration pour le module de communication.
+
+Ce module gère les interactions avec les autres composants du système Le Vieux Moulin.
+"""
+
+from src.integration.system_integrator import SystemIntegrator, get_integrator
+
+__all__ = ["SystemIntegrator", "get_integrator"]
