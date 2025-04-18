7eacdc2694d3200c7d5d1907b563788db0983bcb
Casius999 <casius4126@gmail.com>
2025-04-18 18:00:09 +0200

    feat(accounting): initialisation du package collectors

---
+++ accounting/src/collectors/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package de collecteurs de données pour le module de comptabilité.
+
+Ces collecteurs sont responsables de l'acquisition des données depuis différentes sources:
+- Point de vente (POS)
+- Système de stocks
+- Commandes fournisseurs
+- Système de paie
+- Réservations
+- etc.
+"""
