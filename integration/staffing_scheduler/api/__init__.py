e20973a08c24d2b2a9cb07aa056f3886d82c2f86
Casius999 <casius4126@gmail.com>
2025-04-18 16:23:53 +0200

    Création de la structure du module API

---
+++ integration/staffing_scheduler/api/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Package contenant l'API REST du module de planification du personnel.
+
+Ces modules exposent les fonctionnalités du système de planification via
+une API REST pour l'intégration avec d'autres systèmes.
+"""
+
+from .server import create_app
